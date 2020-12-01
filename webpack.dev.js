0d638c29b4ba84f6b91f3f5e89bd821c80622c5c
jaybee111 <jones.boettcher@gmail.com>
2020-12-01 20:05:23 +0100

    Edit Webpack-Build-Files = Added unminified JavaScript-Version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,95 +1,94 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const libraryName = 'gedepiar';
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
-    devServer: {
-        port: 8080,
-        contentBase: path.join(__dirname, "dist"),
-        hot: true
-    },
-    output: {
-        filename: libraryName+'.js',
-        libraryTarget: 'umd',
-        library: 'gedepiar',
-        libraryExport: 'default',
-        umdNamedDefine: true,
-        globalObject: '(typeof self !== \'undefined\' ? self : this)'
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/index.js',
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+  },
+  output: {
+    filename: `${libraryName}.js`,
+    libraryTarget: 'umd',
+    library: 'gedepiar',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            // creates style nodes from JS strings
+            loader: 'style-loader',
+          },
+          {
+            // translates CSS into CommonJS
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'expanded',
+                sourceMap: true,
+                sourceMapContents: true,
+              },
+              prependData: '@import "variables.scss";',
             },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        // creates style nodes from JS strings
-                        loader: "style-loader"
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            sassOptions: {
-                                outputStyle: 'expanded',
-                                sourceMap: true,
-                                sourceMapContents: true,
-                            },
-                            prependData: '@import "variables.scss";'
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: true
-        })
-    ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]?hash=[hash:20]',
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+    }),
+  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,91 +3,95 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const buildPath = path.resolve(__dirname, 'dist');
 
-const libraryName = 'gedepiar';
+// const libraryName = 'gedepiar';
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: libraryName+'.js',
-        path: buildPath,
-        libraryTarget: 'umd',
-        library: 'gedepiar',
-        libraryExport: 'default',
-        umdNamedDefine: true,
-        globalObject: '(typeof self !== \'undefined\' ? self : this)'
-    },
-    optimization: {
-      minimizer: [new UglifyJsPlugin({
-        parallel: true,
-      })],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
+  entry: {
+    gedepiar: './src/index.js',
+    'gedepiar.min': './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: buildPath,
+    libraryTarget: 'umd',
+    library: 'gedepiar',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      parallel: true,
+    })],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
 
-                options: {
-                    presets: ['@babel/preset-env']
-                }
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.(scss|css|sass)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // translates CSS into CommonJS
+            loader: 'css-loader',
+          },
+          {
+            // Runs compiled CSS through postcss for vendor prefixing
+            loader: 'postcss-loader',
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+              prependData: '@import "variables.scss";',
             },
-            {
-                test: /\.(scss|css|sass)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: 'css-loader',
-                    },
-                    {
-                        // Runs compiled CSS through postcss for vendor prefixing
-                        loader: 'postcss-loader',
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                outputStyle: 'compressed',
-                            },
-                            prependData: '@import "variables.scss";'
-                        }
-                    }
-                ]
+          },
+        ],
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[hash:20].[ext]',
+              limit: 8192,
             },
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[hash:20].[ext]',
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        /*new FaviconsWebpackPlugin({
+          },
+        ],
+      },
+      {
+        // Load all icons
+        test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    /* new FaviconsWebpackPlugin({
             // Your source logo
             logo: './src/assets/icon.png',
             // The prefix for all image files (might be a folder or a name)
@@ -115,22 +119,23 @@ module.exports = {
                 yandex: false,
                 windows: false
             }
-        }),*/
-        new MiniCssExtractPlugin({
-            filename: 'styles.min.css'
-        }),
-        new OptimizeCssAssetsPlugin({
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: {
-                map: {
-                    inline: false,
-                },
-                discardComments: {
-                    removeAll: true
-                },
-                discardUnused: false
-            },
-            canPrint: true
-        })
-    ]
+        }), */
+    new MiniCssExtractPlugin({
+      filename: 'styles.min.css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      // eslint-disable-next-line global-require
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        map: {
+          inline: false,
+        },
+        discardComments: {
+          removeAll: true,
+        },
+        discardUnused: false,
+      },
+      canPrint: true,
+    }),
+  ],
 };
